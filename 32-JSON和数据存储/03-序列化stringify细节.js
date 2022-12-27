4e2db1147bc5d052bf4d3282c50ac6d4cf3184e1
zjx <2252474825@qq.com>
2022-12-27 22:35:10 +0800

    feat: stringify细节

---
+++ 32-JSON和数据存储/03-序列化stringify细节.js
@@ -0,0 +1,30 @@
+const obj = {
+    name: "why",
+    age: 18,
+    friends: {
+        name: "kobe"
+    },
+    hobbies: ["篮球", "足球"]
+}
+// 1.直接转化
+const jsonString1 = JSON.stringify(obj)
+console.log(jsonString1)
+
+// 2.stringify第二个参数replace,可传可不传
+// 2.1 传入数组，设定哪些需要转换
+const jsonString2 = JSON.stringify(obj, ["name", "friends"])
+console.log(jsonString2);//{"name":"why","friends":{"name":"kobe"}}
+
+// 2.2 传入回调函数
+const jsonString3 = JSON.stringify(obj, (key, value) => {
+    if (key === "age") {
+        value = value + 1
+    }
+    return value
+})
+console.log(jsonString3);//{"name":"why","age":19,"friends":{"name":"kobe"},"hobbies":["篮球","足球"]}
+
+// 3.stringify第三个参数 space   了解
+// 对每次转换的结果进行转换数字（1，2）的空格也就是缩进，也可以是字符串
+const jsonString4 = JSON.stringify(obj, null, 2)
+console.log(jsonString4);
